cc719b616324c3e1ee3be791f26fb19d5d9e6427
alperkesen <kesen.alper@gmail.com>
2019-07-17 16:28:34 +0300

    Add cross validation and vocabulary attribute for model

--- src/model.jl
+++ src/model.jl
@@ -4,12 +4,15 @@ mutable struct Model
     config::Config;
     model
     savepath::String
+    vocabulary
 end
 
-Model(config::Config; savedir=SAVEDIR) = Model(config, Chain(), savedir)
+Model(config::Config; savedir=SAVEDIR, voc=nothing) = Model(config, Chain(),
+                                                        savedir, voc)
 Model(inputs::Array{Tuple{String, String},1},
       outputs::Array{Tuple{String, String},1};
-      savedir=SAVEDIR) = Model(Config(inputs, outputs), savedir=SAVEDIR)
+      savedir=SAVEDIR, voc=nothing) = Model(Config(inputs, outputs),
+                                            savedir=SAVEDIR, voc=voc)
 
 getfeatures(m::Model; ftype="all") = getfeatures(m.config; ftype=ftype)
 getfnames(m::Model; ftype="all") = getfnames(m.config; ftype=ftype)
@@ -24,11 +27,51 @@ isimagemodel(m::Model) = in("Image", getftypes(m; ftype="input"))
 istextmodel(m::Model) = in("Text", getftypes(m; ftype="input"))
 
 
-function preparedata(m::Model, traindata; output=true)
+function preprocess(m::Model, data)
+    preprocessed = Dict()
     featurelist = getfeatures(m; ftype="all")
-    trn = preprocess(traindata, featurelist)
-    atype = gpu() >= 0 ? KnetArray{Float64} : Array{Float64}
-    
+    commonfeatures = [(fname, ftype) for (fname, ftype) in featurelist
+                      if in(fname, keys(data))]
+
+    for (fname, ftype) in commonfeatures
+        if ftype == "String"
+            preprocessed[fname] = doc2ids(data[fname])
+        elseif ftype == "Int"
+            preprocessed[fname] = Int.(data[fname])
+        elseif ftype == "Float"
+            if eltype(data[fname]) == String
+                preprocessed[fname] = map(x->parse(Float64, x), data[fname])
+            elseif eltype(data[fname]) == Int
+                preprocessed[fname] = Float64.(data[fname])
+            else
+                preprocessed[fname] = data[fname]
+            end
+        elseif ftype == "Binary Category"
+            if eltype(data[fname]) != Int
+                preprocessed[fname] = map(x->parse(Int64, x), data[fname]) .+ 1
+            else
+                preprocessed[fname] = data[fname] .+ 1
+            end
+        elseif ftype == "Category"
+            preprocessed[fname] = doc2ids(data[fname])
+        elseif ftype == "Image"
+            preprocessed[fname] = [Float64.(readimage(imagepath; dirpath="cifar_100"))
+                                   for imagepath in data[fname]]
+        elseif ftype == "Text"
+            ids, voc = preprocesstext(data[fname]; voc=m.vocabulary)
+            preprocessed[fname] = ids
+            m.vocabulary = voc
+        else
+            preprocessed[fname] = data[fname]
+        end
+    end
+    preprocessed
+end
+
+
+function preparedata(m::Model, traindata; output=true)
+    trn = preprocess(m, traindata)
+
     xtrn = Dict(fname => trn[fname] for fname in getfnames(m; ftype="input"))
 
     if isimagemodel(m)
@@ -39,28 +82,27 @@ function preparedata(m::Model, traindata; output=true)
     else
         catdim = length(size(first(values(xtrn)))) + 1
         xtrn = Array(cat(values(xtrn)..., dims=catdim)')
-        # trn = AutoML.slicematrix(collect(hcat(values(xtrn)...)))
-        xtrn = float(xtrn)
-        xtrn = atype(xtrn)
     end
 
     if output
         ytrn = Dict(fname => trn[fname] for fname in getfnames(m; ftype="output"))
         ytrn = slicematrix(hcat(values(ytrn)...))
-
         return xtrn, ytrn
-    else
-        return xtrn
     end
+
+    return xtrn
 end
 
-function train(m::Model, traindata; epochs=1, batchsize=32, shuffle=true)
+function train(m::Model, traindata; epochs=1, batchsize=32, shuffle=true,
+               cv=false)
+    atype=gpu()>=0 ? KnetArray : Array
     xtrn, ytrn = preparedata(m, traindata)
 
     inputsize = size(xtrn, 1)
     outputsize = size(ytrn, 1)
 
     if !iscategorical(m)
+        xtrn, ytrn = atype(xtrn), atype(ytrn)
         m.model = buildlinearestimator(inputsize, outputsize)
     else
         outputsize = length(unique(ytrn))
@@ -77,13 +119,23 @@ function train(m::Model, traindata; epochs=1, batchsize=32, shuffle=true)
                 m.model = buildquestionmatching(outputsize; pdrop=0.5)
             end
         else
-            m.model = buildclassificationmodel(inputsize, outputsize; pdrop=0.5)
+            xtrn, ytrn = atype(xtrn), atype(ytrn)
+            m.model = buildclassificationmodel(inputsize, outputsize; pdrop=0)
         end
     end
 
-    dtrn = minibatch(xtrn, ytrn, batchsize; shuffle=shuffle)
-    progress!(adam(m.model, repeat(dtrn, epochs)))
-    save(joinpath(SAVEDIR, "model.jld2"), "model", m.model)
+    if cv
+        println("Cross validation")
+        m = crossvalidate(m, xtrn, ytrn; k=10, batchsize=batchsize,
+                          epochs=epochs, shuffle=shuffle)
+        dtrn = minibatch(xtrn, ytrn, batchsize; shuffle=shuffle)
+
+        return m, dtrn
+    else
+        dtrn = minibatch(xtrn, ytrn, batchsize; shuffle=shuffle)
+        progress!(adam(m.model, repeat(dtrn, epochs)))
+        save(joinpath(SAVEDIR, "model.jld2"), "model", m.model)
+    end
     m, dtrn
 end
 
@@ -98,3 +150,35 @@ function predictdata(m::Model, example)
     end
 end
 
+function crossvalidate(m::Model, x, y; k=5, batchsize=32, shuffle=true, epochs=1)
+    xfolds, yfolds = kfolds(x, k), kfolds(y, k)
+    trainacc = 0
+    testacc = 0
+
+    for i=1:k
+        foldxtrn = hcat([xfolds[j] for j=1:k if j != i]...)
+        foldytrn = hcat([yfolds[j] for j=1:k if j != i]...)
+
+        foldxtst = xfolds[i]
+        foldytst = yfolds[i]
+
+        dtrn = minibatch(foldxtrn, foldytrn, batchsize; shuffle=shuffle)
+        dtst = minibatch(foldxtst, foldytst, batchsize; shuffle=shuffle)
+
+        progress!(adam(m.model, repeat(dtrn, epochs)))
+
+        trainacc += accuracy(m.model, dtrn)
+        testacc += accuracy(m.model, dtst)
+    end
+
+    trainacc /= k
+    testacc /= k
+
+    println("Train acc:")
+    println(trainacc)
+
+    println("Test acc:")
+    println(testacc)
+
+    return m
+end
