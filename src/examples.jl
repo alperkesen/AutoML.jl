83fefc394f4b89a44a27e0082cf262f1ee494979
alperkesen <kesen.alper@gmail.com>
2019-07-23 15:19:57 +0300

    Print accuracy in examples after training

--- src/examples.jl
+++ src/examples.jl
@@ -3,7 +3,7 @@ import Random
 using Knet: gpu, KnetArray
 
 function train_house_rentals(; epochs=1)
-    atype=gpu()>=0 ? KnetArray{Float64} : Array{Float64}
+    atype = gpu()>=0 ? KnetArray{Float64} : Array{Float64}
 
     house_rentals = AutoML.house_rentals()
     trn, tst = splitdata(house_rentals; trainprop=0.8)
@@ -42,20 +42,33 @@ function train_gene_sequences(; epochs=1)
     atype=gpu()>=0 ? KnetArray{Float64} : Array{Float64}
 
     gene_sequences = AutoML.splice_junction()
-    gen_trn = AutoML.csv2data(gene_sequences)
+    trn, tst = splitdata(gene_sequences; trainprop=0.8)
+
+    gen_trn = AutoML.csv2data(trn)
+    gen_tst = AutoML.csv2data(tst)
 
     gene_sequences_inputs = [("attribute_$i", "Category") for i in 1:60]
     gene_sequences_outputs = [("Class", "Category")]
 
     model = AutoML.Model(gene_sequences_inputs, gene_sequences_outputs)
-    result = AutoML.train(model, gen_trn; epochs=epochs, cv=true)
+    result = AutoML.train(model, gen_trn; epochs=epochs, cv=false)
 
     xtrn, ytrn = preparedata(model, gen_trn)
     xtrn = atype(xtrn)
 
-    dtrn = minibatch(xtrn, ytrn, 32; shuffle=true)
+    xtst, ytst = preparedata(model, gen_tst)
+    xtst = atype(xtst)
 
-    return model, xtrn, ytrn, dtrn
+    dtrn = minibatch(xtrn, ytrn, model.params["batchsize"]; shuffle=true)
+    dtst = minibatch(xtst, ytst, model.params["batchsize"]; shuffle=true)
+
+    println("Train accuracy:")
+    println(accuracy(model.model, dtrn))
+
+    println("Test accuracy:")
+    println(accuracy(model.model, dtst))
+
+    return model, dtrn, dtst
 end
 
 function train_cifar_100(; smallset=true, epochs=1)
@@ -67,10 +80,21 @@ function train_cifar_100(; smallset=true, epochs=1)
     end
 
     cifar_100_data = AutoML.csv2data(cifar_100)
+
     cifar_100_inputs = [("image_path", "Image")]
     cifar_100_outputs = [("class", "Category")]
+
     model = AutoML.Model(cifar_100_inputs, cifar_100_outputs)
     result = AutoML.train(model, cifar_100_data; epochs=epochs)
+
+    xtrn, ytrn = preparedata(model, cifar_100_data)
+    xtrn = atype(xtrn)
+
+    println("Train accuracy:")
+    println(accuracy(model.model, dtrn))
+
+    println("Test accuracy:")
+    println(accuracy(model.model, dtst))
 end
 
 function train_imdb(; epochs=1)
@@ -86,16 +110,23 @@ function train_imdb(; epochs=1)
     result = AutoML.train(model, imdb_trn; epochs=epochs)
 
     xtrn, ytrn = preparedata(model, imdb_trn)
-    dtrn = minibatch(xtrn, ytrn, 32; shuffle=true)
+    dtrn = minibatch(xtrn, ytrn, model.params["batchsize"]; shuffle=true)
 
     xtst, ytst = preparedata(model, imdb_tst)
-    dtst = minibatch(xtst, ytst, 32; shuffle=true)
+    dtst = minibatch(xtst, ytst, model.params["batchsize"]; shuffle=true)
+
+    println("Train accuracy:")
+    println(accuracy(model.model, dtrn))
+
+    println("Test accuracy:")
+    println(accuracy(model.model, dtst))
 
     model, dtrn, dtst
 end
 
 function train_quora(; epochs=1)
     quora_train, quora_test = AutoML.quora_questions()
+
     quora_trn = AutoML.csv2data(quora_train)
     quora_tst = AutoML.csv2data(quora_test[2:end, :])
 
@@ -106,30 +137,44 @@ function train_quora(; epochs=1)
     result = AutoML.train(model, quora_trn; epochs=epochs)
 
     xtrn, ytrn = preparedata(model, quora_trn)
-    dtrn = minibatch(xtrn, ytrn, 32; shuffle=true)
+    dtrn = minibatch(xtrn, ytrn, model.params["batchsize"]; shuffle=true)
 
     xtst, ytst = preparedata(model, quora_tst)
-    dtst = minibatch(xtst, ytst, 32; shuffle=true)
+    dtst = minibatch(xtst, ytst, model.params["batchsize"]; shuffle=true)
+
+    println("Train accuracy:")
+    println(accuracy(model.model, dtrn))
+
+    println("Test accuracy:")
+    println(accuracy(model.model, dtst))
 
     model, dtrn, dtst
 end
 
 function train_default_of_credit(; epochs=1)
     credit_train, credit_test = AutoML.default_of_credit()
+
     credit_trn = AutoML.csv2data(credit_train)
     credit_tst = AutoML.csv2data(credit_test)
     
     credit_inputs = [(x[1], "Int") for x in credit_trn
                      if x != "default.payment.next.month"]
     credit_outputs = [("default.payment.next.month", "Binary Category")]
+
     model = AutoML.Model(credit_inputs, credit_outputs)
     result = AutoML.train(model, credit_trn; epochs=epochs)
 
     xtrn, ytrn = preparedata(model, credit_trn)
-    dtrn = minibatch(xtrn, ytrn, 32; shuffle=true)
+    dtrn = minibatch(xtrn, ytrn, model.params["batchsize"]; shuffle=true)
 
     xtst, ytst = preparedata(model, credit_tst)
-    dtst = minibatch(xtst, ytst, 32; shuffle=true)
+    dtst = minibatch(xtst, ytst, model.params["batchsize"]; shuffle=true)
+
+    println("Train accuracy:")
+    println(accuracy(model.model, dtrn))
+
+    println("Test accuracy:")
+    println(accuracy(model.model, dtst))
 
     model, dtrn, dtst
 end
