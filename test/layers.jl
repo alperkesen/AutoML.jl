0595181e7ae16d372eda768c86c7ed8dc94f6f6c
alperkesen <kesen.alper@gmail.com>
2019-07-23 12:45:49 +0300

    Add tests for chain

--- test/layers.jl
+++ test/layers.jl
@@ -8,9 +8,9 @@ using Knet: relu, gpu
     output = rand(1:10)
     layer = AutoML.Layer(input, output)
 
-    examplesize = rand(20:30)
-    x = atype(randn(input, examplesize))
-    y = atype(randn(output, examplesize))
+    batchsize = map(x->2^x, rand(3:10))
+    x = atype(randn(input, batchsize))
+    y = atype(randn(output, batchsize))
 
     output = layer(x)
     @test typeof(output) <: atype
@@ -19,3 +19,48 @@ using Knet: relu, gpu
     @test typeof(error) == typeof(float(rand(1:2)))
 end
 
+@testset "linearchain" begin
+    atype = gpu() >= 0 ? KnetArray{Float64} : Array{Float64}
+
+    input = rand(1:10)
+    hidden = rand(1:10)
+    output = rand(1:10)
+
+    layer = AutoML.Layer(input, hidden)
+    layer2 = AutoML.Layer(hidden, output)
+    chain = AutoML.Chain(layer, layer2)
+
+    batchsize = map(x->2^x, rand(3:10))
+    x = atype(randn(input, batchsize))
+    y = atype(randn(output, batchsize))
+
+    output = chain(x)
+    @test typeof(output) <: atype
+
+    error = chain(x, y)
+    @test typeof(error) == typeof(float(rand(1:2)))
+end
+
+@testset "classificationchain" begin
+    atype = gpu() >= 0 ? KnetArray{Float64} : Array{Float64}
+
+    input = rand(1:10)
+    hidden = rand(1:10)
+    output = rand(1:10)
+
+    layer = AutoML.Layer(input, hidden)
+    layer2 = AutoML.Layer(hidden, output)
+    chain = AutoML.Chain2(layer, layer2)
+
+    batchsize = map(x->2^x, rand(3:10))
+    numclass = rand(2:10)
+
+    x = atype(randn(input, batchsize))
+    y = rand([1:numclass;], 1, batchsize)
+
+    output = chain(x)
+    @test typeof(output) <: atype
+
+    error = chain(x, y)
+    @test typeof(error) == typeof(float(rand(1:2)))
+end
