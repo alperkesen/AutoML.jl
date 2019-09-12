b29e09067cdaf309c1c7159d6d252e0f6701865b
alperkesen <kesen.alper@gmail.com>
2019-09-12 16:02:03 +0300

    Add main for training and predicting in one line from command line

--- src/AutoML.jl
+++ src/AutoML.jl
@@ -1,7 +1,12 @@
+using Knet: Knet, AutoGrad
+
 module AutoML
+
+using ArgParse
 
 export
     Knet,
+    AutoGrad,
     Model,
     Config,
     savemodel,
@@ -39,7 +44,6 @@ const DIR = @__DIR__
 const SAVEDIR = abspath(joinpath(DIR, "..", "models"))
 const DATADIR = abspath(joinpath(DIR, "..", "data"))
 
-
 include("config.jl")
 include("layers.jl")
 include("data.jl")
@@ -53,5 +57,68 @@ include("train.jl")
 include("examples.jl")
 include("resnet.jl")
 include("bert.jl")
+
+function main(args)
+    s = ArgParseSettings()
+    s.description = "Automated Machine Learning Tool"
+
+    @add_arg_table s begin
+        ("--config"; default=nothing; help="Yaml file for config")
+        ("--train"; default=nothing; help="Training dataset file.")
+        ("--model_name"; default="model"; help="Model name")
+        ("--model_path"; default=nothing; help="Pretrained model path")
+        ("--predict"; default=nothing; help="Test dataset file.")
+        ("--epochs"; default="1"; help="Number of epochs")
+        ("--optimize"; default="true"; help="Hyperparameter optimization")
+    end
+
+    isa(args, AbstractString) && (args=split(args))
+    println(s.description)
+    o = parse_args(args, s; as_symbols=true)
+
+    istrain = o[:train] != nothing
+    ispredict = o[:predict] != nothing
+
+    if istrain
+        o[:config] == nothing && throw("Config file is missing")
+
+        yamlpath = isabspath(o[:config]) ? o[:config] :
+            joinpath(dirname(DIR), o[:config])
+        trainpath = isabspath(o[:train]) ? o[:train] :
+            joinpath(dirname(DIR), o[:train])
+        epochs = parse(Int, o[:epochs])
+        optimize = parse(Bool, o[:optimize])
+        modelname = o[:model_name]
+
+        inputfeatures, outputfeatures = readyaml(yamlpath)
+        model = Model(inputfeatures, outputfeatures; name=modelname)
+        model, dtrn = train(model, trainpath;
+                            epochs=epochs,
+                            showprogress=false,
+                            issave=true,
+                            optimize=optimize)
+
+        println("Error: ", model.model(dtrn.x, dtrn.y))
+        println("Accuracy: ", accuracy(model.model, dtrn))
+bo
+        return
+    elseif ispredict
+        o[:model_path] == nothing && throw("Model path is missing")
+
+        modelpath = isabspath(o[:model_path]) ? o[:model_path] :
+            joinpath(dirname(DIR), o[:model_path])
+        testpath = isabspath(o[:predict]) ? o[:predict] :
+            joinpath(dirname(DIR), o[:predict])
+
+        model = loadmodel(modelpath)
+        dtst = getbatches(model, testpath; showtime=false)
+        println("Error: ", model.model(dtst.x, dtst.y))
+        println("Accuracy: ", accuracy(model.model, dtst))
+    else
+        throw("Use --train or --predict parameters to use package")
+    end
+end
+
+basename(PROGRAM_FILE) == "AutoML.jl" && main(ARGS)
 
 end
